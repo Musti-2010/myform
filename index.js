37da44740420ed5d51837cfbb543066f155e0645
Musti-2010 <109356143+Musti-2010@users.noreply.github.com>
2022-07-16 09:14:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const a = document.getElementById('myform');
+let b = document.getElementById('textfield');
+const c = document.getElementById('myvalid');
+let g = document.getElementById("yourid");
+let my = document.getElementById("mypassword");
+const num = (/^[0-9]+$/);
+let tel = document.getElementById('myphone');
+
+function Validrom(){
+ 
+ var returnval = true
+ if( c.innerText != b.value && g.innerText != my.value){
+
+returnval = false
+
+};
+
+
+
+
+ 
+
+  
+
+ return returnval;
+
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
